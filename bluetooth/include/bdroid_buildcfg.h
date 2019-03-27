5c4ed0077410d77a66d4df7fe6f9fd5f5855972c
Nitin Shivpure <nshivpur@codeaurora.org>
2019-03-27 18:45:38 +0530

    picasso: Switch to bdroid_buildcfg.h for qti bt stack
    - qva configuration is used for qti bt stack.
    
    Change-Id: I5d930c62633fc768bba7bf5ad2ae24e6b2874072

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -22,13 +22,9 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 #define BTM_DEF_LOCAL_NAME   "Redmi K30 5G"
-// Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    16
+#define MAX_L2CAP_CHANNELS    32
 #define BLE_VND_INCLUDED   TRUE
-// skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
-
-/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
-#define AVDT_NUM_SEPS 12
+#define AVDT_NUM_SEPS 35
 #endif
