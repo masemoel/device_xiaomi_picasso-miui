dbad716a2f25365a2ceb97a7deb57882bee91e44
baalajimaestro <me@baalajimaestro.me>
2020-07-25 19:03:29 +0530

    picasso: Kang dexpreopt bits from OnePlus 7 tree
    Speeds up first boot insanely
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,14 @@
+# ART
+PRODUCT_PROPERTY_OVERRIDES += \
+    pm.dexopt.ab-ota=speed-profile \
+    dalvik.vm.dex2oat-filter=speed \
+    dalvik.vm.image-dex2oat-filter=speed \
+    ro.vendor.qti.am.reschedule_service=true \
+    ro.sys.fw.dex2oat_thread_count=8 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.dex2oat-threads=4 \
+    dalvik.vm.image-dex2oat-threads=4
+
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.btstack.enable.swb=true \
