5da1312934ff267c0f60353a3255a352910aa777
Alex Yustasov <alex9yust@gmail.com>
2020-07-31 10:51:55 +0300

    git pushpicasso: kernel from xiaomi.eu 20.7.30

--- device.mk
+++ device.mk
@@ -124,3 +124,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/modules-20.7.30,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/lib/modules)
+
