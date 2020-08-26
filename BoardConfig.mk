3fc626a73bd20019c3755a6cca9fdc05415b0a93
masemoel <az.az.az.mjme@gmail.com>
2020-08-26 13:08:52 +0200

    picasso: call vendor

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,3 +130,6 @@ BOARD_VNDK_VERSION := current
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
+# Inherit from the proprietary version
+-include vendor/xiaomi/picasso/BoardConfigVendor.mk
