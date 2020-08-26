fd9822f84fa1e597c17f852b7dcb5473cb49813f
masemoel <az.az.az.mjme@gmail.com>
2020-08-26 13:10:08 +0200

    picasso: add FM and HIDL

--- device.mk
+++ device.mk
@@ -78,6 +78,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
+# FM
+PRODUCT_PACKAGES += \
+    FM2 \
+    libqcomfm_jni \
+    qcom.fmradio
+
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc
