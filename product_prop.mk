dae3d18b2b3f0c0e344dbde0c3bf260a1e612a12
masemoel <az.az.az.mjme@gmail.com>
2020-09-03 16:01:25 +0200

    picasso: Revert: "add aux lens support for all GCams"

--- product_prop.mk
+++ product_prop.mk
@@ -14,7 +14,7 @@ endif
 
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
-    vendor.camera.aux.packagelist=org.lineageos.snap,net.sourceforge.opencamera,org.codeaurora.snapcam,com.google.android.GoogleCamera
+    vendor.camera.aux.packagelist=org.lineageos.snap,net.sourceforge.opencamera,org.codeaurora.snapcam
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.sf.lcd_density=375
