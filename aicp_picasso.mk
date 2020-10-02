cd63de9ec03f64ef7707545da4ad66e12312382c
masemoel <az.az.az.mjme@gmail.com>
2020-10-02 17:42:37 +0200

    Add ANXcamera to build
    Signed-off-by: masemoel <az.az.az.mjme@gmail.com>

--- aicp_picasso.mk
+++ aicp_picasso.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# ANXCamera
+$(call inherit-product, vendor/ANXCamera/config.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
