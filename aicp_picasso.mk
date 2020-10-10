ae427f3bfe5519b3771a7546d3eb6fdd26f15253
masemoel <az.az.az.mjme@gmail.com>
2020-10-10 18:14:19 +0200

    Revert "picasso: Remove unnecessary inherits"
    This reverts commit 7360ff09f3a3a98f8a2b704ea29a38fbbfa18390.

--- aicp_picasso.mk
+++ aicp_picasso.mk
@@ -5,7 +5,11 @@
 #
 
 # ANXCamera
-$(call inherit-product-if-exists, vendor/ANXCamera/config.mk)
+$(call inherit-product, vendor/ANXCamera/config.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from picasso device
 $(call inherit-product, device/xiaomi/picasso/device.mk)
