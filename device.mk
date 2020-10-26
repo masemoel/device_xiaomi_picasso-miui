917083cd40d29e3ff9c99b66d43a4d1d23a1c484
Jeong Seong Pil <jjjhitel@gmail.com>
2020-10-27 02:45:27 +0900

    picasso: Move overlay-aicp to overlay
    Signed-off-by: masemoel <az.az.az.mjme@gmail.com>

--- device.mk
+++ device.mk
@@ -107,8 +107,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aicp
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     NotchNoFillOverlay
