b9c8d6422113e020dbb910fc080325170914e6e0
Jeong Seong Pil <jjjhitel@gmail.com>
2020-10-27 18:17:41 +0900

    picasso: Add hotwordenrollment support
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@gmail.com>

--- device.mk
+++ device.mk
@@ -80,6 +80,11 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
+# Hotword Enrollment
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc
