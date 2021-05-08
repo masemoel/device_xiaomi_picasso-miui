372c5a912fe97cdcca2724154f8288bf9f6a083b
HinataKato <57648531+HinataKato@users.noreply.github.com>
2021-05-08 18:19:21 +0800

    picasso: init: Update fingerprint to coral May 2021

--- init/init_picasso.cpp
+++ init/init_picasso.cpp
@@ -66,11 +66,11 @@ constexpr const char *DEVICES[] = {
 };
 
 constexpr const char *BUILD_DESCRIPTION[] = {
-    "coral-user 11 RQ2A.210405.005 7181113 release-keys",
+    "coral-user 11 RQ2A.210505.002 7246365 release-keys",
 };
 
 constexpr const char *BUILD_FINGERPRINT[] = {
-    "google/coral/coral:11/RQ2A.210405.005/7181113:user/"
+    "google/coral/coral:11/RQ2A.210505.002/7246365:user/"
     "release-keys",
 };
 
