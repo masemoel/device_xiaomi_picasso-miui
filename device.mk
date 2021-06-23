de646435b7e20556ad9fba115e0e5cefa265086e
Manuel José Moral Eliche <az.az.az.mjme@gmail.com>
2021-06-23 23:53:40 +0200

    picasso: Include hotword blobs
    - This repo must be cloned: https://github.com/chiru2000/vendor_xiaomi_google

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Hotword blobs
+$(call inherit-product, vendor/xiaomi/google/google.mk)
+
 # MIUI Camera
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 
