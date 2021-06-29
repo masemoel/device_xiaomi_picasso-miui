6c4ad80a87f989a995d0051aad597599e5d5b316
Manuel José Moral Eliche <az.az.az.mjme@gmail.com>
2021-06-30 00:34:45 +0200

    picasso: Add missing call for gpio-keys.kl

--- device.mk
+++ device.mk
@@ -152,6 +152,7 @@ PRODUCT_COPY_FILES += \
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
 
