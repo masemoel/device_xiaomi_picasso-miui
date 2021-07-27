ded83467f5284e9b03431a146db290c1c3f11062
masemoel <az.az.az.mjme@gmail.com>
2021-07-27 23:25:42 +0200

    picasso: Update prebuilt images to weekly 21.7.21 EU release
    Signed-off-by: masemoel <az.az.az.mjme@gmail.com>

--- device.mk
+++ device.mk
@@ -174,7 +174,7 @@ PRODUCT_COPY_FILES += \
 
 # Modules
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/modules-21.7.14,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/lib/modules)
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/modules-21.7.21,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/lib/modules)
     
 # NFC
 PRODUCT_PACKAGES += \
