d911e3bd9169384d7355b544f6f349805b831cf9
masemoel <az.az.az.mjme@gmail.com>
2021-09-19 12:56:38 +0200

    picasso: Update prebuilt images to stable CN 12.5.5 release

--- device.mk
+++ device.mk
@@ -176,7 +176,7 @@ PRODUCT_COPY_FILES += \
 
 # Modules
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/modules-21.8.4,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/lib/modules)
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/prebuilt/modules-12.5.5,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/lib/modules)
     
 # NFC
 PRODUCT_PACKAGES += \
