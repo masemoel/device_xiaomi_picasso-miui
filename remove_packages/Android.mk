d9524d43ae36befdd9eb5f18089845b51b0e65b3
masemoel <az.az.az.mjme@gmail.com>
2021-09-21 23:11:34 +0200

    picasso: Don't remove GCamGoPrebuilt

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := ViaBrowser SimpleGalleryPro TurboPrebuilt GCamGOPrebuilt GoogleOneTimeInitializer WellbeingPrebuilt
+LOCAL_OVERRIDES_PACKAGES := ViaBrowser SimpleGalleryPro TurboPrebuilt GoogleOneTimeInitializer WellbeingPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
