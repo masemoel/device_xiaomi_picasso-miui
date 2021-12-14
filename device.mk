39eb6c3bbbdea429867495dc480fb2f4d00554be
masemoel <az.az.az.mjme@gmail.com>
2021-12-14 18:29:43 +0100

    picasso: Remove AICP leftovers

--- device.mk
+++ device.mk
@@ -223,10 +223,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-parts.xml
-    
-# PartsBin
-PRODUCT_PACKAGES += \
-    PartsBin
 
 # Permissions
 PRODUCT_COPY_FILES += \
